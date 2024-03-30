49c65c73cedad324d6bd9da49771b9e6451f31f0
Anh Sky Dev <anhhh11345@gmail.com>
2024-03-30 10:26:15 +0700

    html-css first commit

---
+++ JAVASCRIPT/Bai_15__String_Methods/main.js
@@ -0,0 +1,19 @@
+/**
+ *  Làm việc với chuỗi
+ * 
+ */
+
+//  Keywork : Javascript String Methods
+
+//  1.  Length
+//  2.  Find Index
+//  3.  Cut String
+//  4.  Relape
+//  5.  Convert to upper case
+//  6.  Conver to lower case
+//  7.  Trim
+//  8.  Split
+//  9.  Get a character by index
+
+var myString =  'Nguyễn Thế Anh';
+console.log(myString.length);
